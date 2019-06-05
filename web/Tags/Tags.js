f68f260d130084c050c9b617098fd6f0395a9d04
Steel-Collin <k_swatkins@hotmail.com>
2019-06-04 20:28:06 -0600

    added nasa api and resulting pic

--- web/Tags/Tags.js
+++ web/Tags/Tags.js
@@ -5,3 +5,18 @@ function startup(){
 function displayStuff(){
     document.getElementById('display2').innerHTML = "This is from with in the javascript file.";
 }
+
+function retrieveImages(){
+    $.ajax({
+        url: 'https://api.nasa.gov/planetary/apod?api_key=hmTyoJjCdzmIQ3Y9dAYF0tOTC7FqFwaGOMW1eMnk',
+        success: function (data) {
+            var viewImage = "<img id='nasa1' width='350' src='" + data.hdurl + "'></br><p>" + data.explanation 
+            + "</p>";
+           document.getElementById('display2').innerHTML = viewImage;
+        },
+        error: function () {
+           alert("ERROR!");
+  
+        }
+     });
+}
