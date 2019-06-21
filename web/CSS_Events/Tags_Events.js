e6cb14e66b234ee538bc3a62c8f2dadda0b7c0c6
Steel-Collin <k_swatkins@hotmail.com>
2019-06-20 19:42:18 -0600

    Added animation page

--- web/CSS_Events/Tags_Events.js
+++
@@ -1,7 +0,0 @@
-function startup(){
-    document.getElementById('header').addEventListener('click', displayStuff);
-}
-
-function displayStuff(){
-    document.getElementById('display2').innerHTML = "This is from with in the javascript file.";
-}
---
+++ web/CSS_TransitionAnimationTransform/Animation.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>CIT261</title>
+   <meta name="viewport" content="width=device-width, initial-scale1" />
+   <link href="Trans_Trans_Auto.css" rel="stylesheet">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+   <script type="text/javascript" src="Trans_Trans_Auto.js"></script>
+   <script type="application/json" src="Trans_Trans_Auto.js"></script>
+</head>
+<body id="body">
+    <div id="headContainer" class="headContainer">
+    </div>
+</body>
+</html>
